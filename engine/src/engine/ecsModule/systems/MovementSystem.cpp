2e640c5ae1f40fa545a4d3f82e6688f418c3bd4a
Vladimir Glushkov <glushkov.vk@students.dvfu.ru>
2023-02-01 04:52:22 +0600

    Add MovementSystem draft

---
+++ engine/src/engine/ecsModule/systems/MovementSystem.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Vladimir on 01.02.2023.
+//
+
+#include "MovementSystem.h"
+
+namespace ecs = pce::ecs;
+using namespace ecs::systems;
+
+MovementSystem::MovementSystem() {
+	//TODO: add require components Transform and Velocity
+}
+
+void MovementSystem::Update(float deltaTime) {
+
+}
