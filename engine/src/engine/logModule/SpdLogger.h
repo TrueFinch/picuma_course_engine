a23e238d5cf329420dbd812fee347720ffb50007
Vladimir Glushkov <glushkov.vk@students.dvfu.ru>
2023-01-31 18:56:39 +0600

    Add SpdLogger

---
+++ engine/src/engine/logModule/SpdLogger.h
@@ -0,0 +1,14 @@
+//
+// Created by Vladimir on 31.01.2023.
+//
+
+#ifndef PICUMA_COURSE_ENGINE_SPDLOGGER_H
+#define PICUMA_COURSE_ENGINE_SPDLOGGER_H
+
+
+class SpdLogger {
+
+};
+
+
+#endif //PICUMA_COURSE_ENGINE_SPDLOGGER_H
