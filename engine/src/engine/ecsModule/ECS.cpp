a8ada8e23dcf9403586e77b1a6c83f21e14791cd
Vladimir Glushkov <glushkov.vk@students.dvfu.ru>
2023-02-02 01:00:59 +0600

    ECS: implement Entity, Component and System classes

--- engine/src/engine/ecsModule/ECS.cpp
+++ engine/src/engine/ecsModule/ECS.cpp
@@ -2,4 +2,27 @@
 // Created by Vladimir on 01.02.2023.
 //
 
+#include <algorithm>
+
 #include "ECS.h"
+
+using namespace pce::ecs;
+using namespace pce::utilsModule;
+
+void System::AddEntity(std::weak_ptr<Entity> entity) {
+	m_entities.emplace_back(entity);
+}
+
+void System::RemoveEntity(std::weak_ptr<Entity> entity) {
+	m_entities.erase(std::remove_if(m_entities.begin(), m_entities.end(), [&entity](std::weak_ptr<Entity> el) {
+		return *el.lock() == *entity.lock();
+	}), m_entities.end());
+}
+
+std::vector<std::weak_ptr<Entity>> System::GetEntities() const {
+	return m_entities;
+}
+
+const Signature& System::GetComponentSignature() const {
+	return m_componentSignature;
+}
--- engine/src/engine/ecsModule/ECS.h
+++ engine/src/engine/ecsModule/ECS.h
@@ -6,22 +6,92 @@
 
 #include <vector>
 #include <memory>
+#include <bitset>
+#include <unordered_map>
 
 #include "utilsModule/KeyId.h"
 
 namespace pce::ecs {
-	class Entity : utilsModule::UniqueIdProvider<Entity> {
+	class Entity : public utilsModule::UniqueIdProvider<Entity> {
+	public:
+		explicit Entity() = default;
+
+		explicit Entity(Entity::Id id) : utilsModule::UniqueIdProvider<Entity>(id) {}
+
+		Entity(const Entity& other) noexcept = default;
+
+		Entity(Entity&& other) noexcept = default;
+
+		~Entity() override = default;
+
+		Entity& operator=(const Entity& other) noexcept = default;
+
+		Entity& operator=(Entity&&) noexcept = default;
+
+		bool operator==(const Entity& other) noexcept { return GetId() == other.GetId(); }
+
+		bool operator!=(const Entity& other) noexcept { return GetId() != other.GetId(); }
+
+		bool operator<(const Entity& other) noexcept { return GetId() < other.GetId(); }
+
+		bool operator>(const Entity& other) noexcept { return GetId() > other.GetId(); }
+
 		virtual void Setup() {};
+
 		virtual void Update(float deltaTime) {};
+
 		virtual void Teardown() {};
 	};
 
-	class Component {
-		// TODO implement
+	const uint32 MAX_COMPONENTS = 32;
+	typedef std::bitset<MAX_COMPONENTS> Signature;
+
+	//TODO make class abstract
+	class IComponent : public utilsModule::UniqueIdProvider<IComponent> {};
+
+	template<typename T>
+	class Component : public utilsModule::UniqueIdProvider<Component<T>> {
+	public:
+		using TypeId = utilsModule::UniqueIdProvider<IComponent>::Id;
+		using ObjId = typename utilsModule::UniqueIdProvider<Component<T>>::Id;
+
+		[[nodiscard]] TypeId GetTypeId() const noexcept {
+			return utilsModule::UniqueIdProvider<IComponent>::GetId();
+		}
+
+		[[nodiscard]] ObjId GetId() const noexcept {
+			return utilsModule::UniqueIdProvider<IComponent>::GetId();
+		}
 	};
 
-	class System : Entity {
-		// TODO implement
+	/***
+	 * System is an entity that manages other entities
+	 * that has specific associated components
+	 */
+	class System : public Entity {
+	public:
+		System() = default;
+
+		~System() override = default;
+
+		void AddEntity(std::weak_ptr<Entity> entity);
+
+		void RemoveEntity(std::weak_ptr<Entity> entity);
+
+		[[nodiscard]] std::vector<std::weak_ptr<Entity>> GetEntities() const;
+
+		[[nodiscard]] const Signature& GetComponentSignature() const;
+
+		template<typename T>
+		void RequireComponent() {
+			const auto id = Component<T>::GetTypeId();
+			m_componentSignature.set(id);
+		}
+
+	private:
+		Signature m_componentSignature;
+
+		std::vector<std::weak_ptr<Entity>> m_entities;
 	};
 
 	class Registry {
